b8cbff8fd7adac349c544c55ba1a23a0753b01ad
Medhi Raza <m.jaffri@sellanycar.com>
2020-12-01 17:44:20 +0400

    Copy Build

---
+++ precache-manifest.630f14c95cefbfdabd1da48e5f2e5983.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cce05e4dc18a94529d986a96ce62e25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0a324201ede63637631",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "5d7c1f7a369803dd2f8f",
+    "url": "/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "7035e0aca03f9f1da1be",
+    "url": "/static/css/main.bd4e58e4.chunk.css"
+  },
+  {
+    "revision": "c8fec297de2dda5eb28f",
+    "url": "/static/js/2.dc42b324.chunk.js"
+  },
+  {
+    "revision": "a0a324201ede63637631",
+    "url": "/static/js/3.8d08667c.chunk.js"
+  },
+  {
+    "revision": "5d7c1f7a369803dd2f8f",
+    "url": "/static/js/4.fb2822ac.chunk.js"
+  },
+  {
+    "revision": "7035e0aca03f9f1da1be",
+    "url": "/static/js/main.5fc0bdc6.chunk.js"
+  },
+  {
+    "revision": "3b59a60f5f091a2e90ba",
+    "url": "/static/js/runtime-main.0a8cb6f0.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "d565e0e3aff8dd51d9e2c17a73c36fde",
+    "url": "/static/media/download.d565e0e3.bin"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "83a05b10639d238bd8c5bbab279ab911",
+    "url": "/static/media/harvardLogo.83a05b10.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "39c1d4910d3b8dd19b37eae89ff4b9e9",
+    "url": "/static/media/sac.39c1d491.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "2fd008c708437fb5d030056e4f2bee5d",
+    "url": "/static/media/vd.2fd008c7.svg"
+  }
+]);
