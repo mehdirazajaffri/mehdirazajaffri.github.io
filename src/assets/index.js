d1bc76b48e75f73913fc229f1f396778648170d2
Mehdi Raza <mr@berkeley-assets.com>
2023-05-29 16:47:16 +0400

    fixing icons and fonts

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,60 +6,61 @@ import worldmap from './backgrounds/world-map.png';
 import logo from './logo/logo-black.png';
 import logotext from './logo/logo-text-black.png';
 
-import shaq from './personal/shaq.png';
+import mehdiAvatar from './personal/mehdi-avatar.png';
 import mehdi from './personal/mehdi.png';
+import shaq from './personal/shaq.png';
 
 import backend from './icons/backend.png';
-import ux from './icons/ux.png';
-import frontend from './icons/frontend.png';
-import prototyping from './icons/prototyping.png';
-import github from './icons/github.png';
 import close from './icons/close.png';
 import download from './icons/download.png';
 import downloadHover from './icons/downloadHover.png';
+import frontend from './icons/frontend.png';
+import github from './icons/github.png';
+import menu from './icons/menu.png';
 import pineapple from './icons/pineapple.png';
 import pineappleHover from './icons/pineappleHover.png';
-import menu from './icons/menu.png';
+import prototyping from './icons/prototyping.png';
 import resume from './icons/resume.png';
 import send from './icons/send.png';
 import sendHover from './icons/sendHover.png';
+import ux from './icons/ux.png';
 
 import css from './tech/css.png';
 import docker from './tech/docker.png';
 import figma from './tech/figma.png';
 import git from './tech/git.png';
+import graphql from './tech/graphql.png';
 import html from './tech/html.png';
 import javascript from './tech/javascript.png';
 import nodejs from './tech/nodejs.png';
+import postgresql from './tech/postgresql.png';
 import reactjs from './tech/reactjs.png';
 import redux from './tech/redux.png';
+import rubyrails from './tech/rubyrails.png';
 import tailwind from './tech/tailwind.png';
 import typescript from './tech/typescript.png';
-import graphql from './tech/graphql.png';
-import postgresql from './tech/postgresql.png';
-import rubyrails from './tech/rubyrails.png';
 
 import coverhunt from './company/coverhunt.png';
-import venturedive from './company/venturedive.png';
 import dcc from './company/dcc.png';
 import kelhel from './company/kelhel.png';
 import microverse from './company/microverse.png';
+import venturedive from './company/venturedive.png';
 
+import aws from './icons/aws.png';
+import django from './icons/django.png';
+import python from './icons/python.png';
+import berkeley from './projects/berkeley.png';
 import komikult from './projects/komikult.png';
 import leaderboard from './projects/leaderboard.png';
 import math from './projects/math-magicians.png';
 import movie from './projects/movie-metro.png';
 import nyeusi from './projects/nyeusi.png';
-import space from './projects/space-hub.png';
-import python from './icons/python.png';
-import aws from './icons/aws.png';
-import django from './icons/django.png';
-import servicemart from './projects/servicemarket.png';
-import zglue from './projects/zglue.png';
-import vistajet from './projects/vistajet.png';
 import sellanycar from './projects/sellanycar.png';
-import berkeley from './projects/berkeley.png';
+import servicemart from './projects/servicemarket.png';
+import space from './projects/space-hub.png';
+import vistajet from './projects/vistajet.png';
 import xbyoj from './projects/xbyoj.png';
+import zglue from './projects/zglue.png';
 
 export {
   bwmap,
@@ -118,4 +119,5 @@ export {
   sellanycar,
   berkeley,
   xbyoj,
+  mehdiAvatar
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,6 @@
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import { bwmap, mehdiAvatar, worldmap } from '../assets';
 import { styles } from '../styles';
-import { navLinks } from '../constants';
-import { shaq, bwmap, worldmap, mehdi } from '../assets';
 
 const Hero = () => {
   return (
@@ -84,10 +82,10 @@ const Hero = () => {
         {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
         <div>
           <img
-            className="absolute bottom-0 ml-[50vw] 
+            className="absolute bottom-0 ml-[35vw] 
             lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
             sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
-            src={mehdi}
+            src={mehdiAvatar}
             alt="Mehdi Raza"
           />
         </div>
